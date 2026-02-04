640f547755fca303d607e622bba8c115b1ad1567
Laurent Cordier <laurent.cordier28@orange.fr>
2026-02-04 16:25:25 +0100

    Intégration de la carte Leaflet et des marqueurs POI

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,46 +1,81 @@
+/*
+  MapContainer : pour créer la carte LeafLet.
+  TileLayer : pour afficher le fond de la carte avec OpenStreetMap.
+  Marker : pour ajouter un marqueur sur la carte.
+  Popup : pour afficher une bulle d'information lors du clic sur le marqueur.
+*/
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
 function Map({ currentCity, poiList, onPOISelect }) {
-
-  const renderPOIList = () => {
-    if (poiList.length === 0) return null;
-
-    return (
-      <ul>
-        {poiList.map((poi) => (
-          <li
-            key={poi.id}
-            onClick={() => onPOISelect(poi)}
-            style={{
-              cursor: "pointer",
-            }}
-          >
-           <p>
-              <strong>Id : </strong>{poi.id} | {" "}
-              <strong>Nom :</strong>{poi.name} | {" "}
-              <strong>Adresse :</strong>{poi.address} | {" "}
-              <strong>Lat :</strong>{poi.lat} | {" "}
-              <strong>Lon :</strong>{poi.lon} | {" "}
-              <strong>Description :</strong>{poi.description}
-            </p>
-            <br />
-          </li>
-        ))}
-      </ul>
-    );
-  };
+  //console.log("Map render - currentCity:", currentCity);
+  /*
+  Leaflet attend des chiffres pour lat et lon.
+  On centre sur la ville choisie sinon Paris par défaut.
+  */
+  const center = currentCity ? [Number(currentCity.lat), Number(currentCity.lon)] : [48.8566, 2.3522];
 
   return (
     <>
-      <h2>MAP(affichage de la carte)</h2>
-      {currentCity ? (
-        <>
-          <p>centrage à faire sur la ville : {currentCity.name}</p>
-          <h3>Liste des points d'intérêts à afficher sous forme de marqeurs : </h3>
-          {renderPOIList()}
-        </>
-      ) : (
-        <p>Aucune ville sélectionnée</p>
-      )}
+      <h2>MAP(affichage Leaflet)</h2>
+
+      {/* Carte */}
+      <MapContainer
+        className="map"
+        center={center}
+        // astuce pour que la carte se recentre à chaque changement de currentCity (à priori moins efficace, à voir).
+        key={currentCity?.id ?? "default"}
+        /*
+        6 = pays
+        12 = ville
+        15 = quartier
+        */
+        zoom={13}
+        // si besoin de supprimer le zoom avec la molette.
+        // scrollWheelZoom={false}
+      >
+
+        {/* fond OpenStreetMap */}
+        <TileLayer
+          // obligation légale (affichage en bas à droite)
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          /*
+          Pour charger les images, Leaflet a besoin de savoir chez quel fournisseur récupérer l'image (ici OpenStreetMap).
+          Il génère automatiquement ces valeurs :
+            {z} = niveau de zoom
+            {x} {y} = coordonnées de la tuile
+            {s} = serveur (a,b,c)
+          */
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {/* Marqueur indiquant la ville choisie ==> non demandé dans la maquette Figma*/}
+        {/*
+        <Marker position={center}>
+          <Popup>
+            {currentCity ? currentCity.name : "Paris (défaut)"}
+          </Popup>
+        </Marker>
+        */}
+
+        {/* Marqueurs indiquant les différents points d'intérêt (Macdo) */}
+        {poiList.map((poi) => (
+
+          <Marker
+            key={poi.id}
+            position={[Number(poi.lat), Number(poi.lon)]}
+          >
+            <Popup>
+              <strong>{poi.name}</strong>
+              <br />
+              {poi.address}
+              <br />
+              <button onClick={() => onPOISelect(poi)}>Choisir</button>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </>
   );
-};
+}
+
 export default Map;
